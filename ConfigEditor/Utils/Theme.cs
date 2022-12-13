734d7100c7a6762b427acecfdd31a5963310685d
warquys <64769541+warquys@users.noreply.github.com>
2022-12-13 16:29:35 +0100

    Auto Drak Theme & pre Server Consol

---
+++ ConfigEditor/Utils/Theme.cs
@@ -0,0 +1,26 @@
+﻿using DevExpress.LookAndFeel;
+using DevExpress.Skins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigEditor.Utils
+{
+    internal class Theme
+    {
+        [DllImport("UXTheme.dll", SetLastError = true, EntryPoint = "#138")]
+        public static extern bool ShouldSystemUseDarkMode();
+
+        public static void SetSkin(uint index)
+        {
+            List<SkinContainer> list = SkinManager.Default.Skins.Cast<SkinContainer>().ToList();
+            if (index >= 0 && index < list.Count())
+            {
+                UserLookAndFeel.Default.SetSkinStyle(list[(int)index].SkinName);
+            }
+        }
+    }
+}
